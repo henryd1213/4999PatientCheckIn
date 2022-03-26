f2f4d04581d5a23ad514982a480f56bb005a8dd9
henryd1213 <78320788+henryd1213@users.noreply.github.com>
2022-03-26 13:59:31 -0400

    Update PatientInfo.sql

--- PatientInfo.sql
+++ PatientInfo.sql
@@ -8,7 +8,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema PatientInfo
 -- -----------------------------------------------------
 
-
 -- -----------------------------------------------------
 -- Schema PatientInfo
 -- -----------------------------------------------------
@@ -24,10 +23,11 @@ CREATE TABLE IF NOT EXISTS `PatientInfo`.`Form_List` (
   `Author` VARCHAR(45) NOT NULL,
   `Creation` VARCHAR(45) NOT NULL,
   `Apperance` INT NOT NULL,
-  `Metadata` VARCHAR(45) NULL,
-  `Use` VARCHAR(45) NULL,
+  `Metadata` VARCHAR(45) NULL DEFAULT NULL,
+  `Use` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -47,6 +47,7 @@ CREATE TABLE IF NOT EXISTS `PatientInfo`.`Employee_Info` (
   `Age` VARCHAR(45) NOT NULL,
   `FPass` VARCHAR(45) NOT NULL,
   `Form_List_ID` INT NOT NULL,
+  `LogLim` INT NULL,
   PRIMARY KEY (`ID`, `Form_List_ID`),
   UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
   UNIQUE INDEX `Username_UNIQUE` (`Username` ASC) VISIBLE,
@@ -54,48 +55,9 @@ CREATE TABLE IF NOT EXISTS `PatientInfo`.`Employee_Info` (
   INDEX `fk_Employee_Info_Form_List1_idx` (`Form_List_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Employee_Info_Form_List1`
     FOREIGN KEY (`Form_List_ID`)
-    REFERENCES `PatientInfo`.`Form_List` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `PatientInfo`.`Patient_Info`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `PatientInfo`.`Patient_Info` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `Fname` VARCHAR(45) NOT NULL,
-  `Lname` VARCHAR(45) NOT NULL,
-  `Street` VARCHAR(45) NOT NULL,
-  `AptFloorSuite` VARCHAR(45) NULL,
-  `City` VARCHAR(45) NOT NULL,
-  `State` VARCHAR(45) NOT NULL,
-  `Zip` VARCHAR(45) NOT NULL,
-  `Phone` VARCHAR(45) NOT NULL,
-  `Email` VARCHAR(45) NOT NULL,
-  `Pref_contact` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `PatientInfo`.`Pin_Info`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `PatientInfo`.`Pin_Info` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `Pinname` VARCHAR(45) NOT NULL,
-  `Pin` VARCHAR(45) NOT NULL,
-  `Employee_Info_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`, `Employee_Info_ID`),
-  INDEX `fk_Pin_Info_Employee_Info1_idx` (`Employee_Info_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_Pin_Info_Employee_Info1`
-    FOREIGN KEY (`Employee_Info_ID`)
-    REFERENCES `PatientInfo`.`Employee_Info` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `PatientInfo`.`Form_List` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -108,21 +70,41 @@ CREATE TABLE IF NOT EXISTS `PatientInfo`.`Form_Questions` (
   `Q_apperance` VARCHAR(45) NOT NULL,
   `Form_List_ID` INT NOT NULL,
   `Date` VARCHAR(45) NOT NULL,
-  `imageUpload` BLOB NULL,
-  `Radioone` VARCHAR(45) NULL,
-  `Radiotwo` VARCHAR(45) NULL,
-  `Radiothree` VARCHAR(45) BINARY NULL,
-  `Radiofour` VARCHAR(45) NULL,
-  `Radiofive` VARCHAR(45) NULL,
-  `RadioSix` VARCHAR(45) NULL,
+  `imageUpload` VARCHAR(255) NULL DEFAULT NULL,
+  `Radioone` VARCHAR(45) NULL DEFAULT NULL,
+  `Radiotwo` VARCHAR(45) NULL DEFAULT NULL,
+  `Radiothree` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL,
+  `Radiofour` VARCHAR(45) NULL DEFAULT NULL,
+  `Radiofive` VARCHAR(45) NULL DEFAULT NULL,
+  `RadioSix` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`, `Form_List_ID`),
   INDEX `fk_Form_Questions_Form_List1_idx` (`Form_List_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Form_Questions_Form_List1`
     FOREIGN KEY (`Form_List_ID`)
-    REFERENCES `PatientInfo`.`Form_List` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `PatientInfo`.`Form_List` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `PatientInfo`.`Patient_Info`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PatientInfo`.`Patient_Info` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Fname` VARCHAR(45) NOT NULL,
+  `Lname` VARCHAR(45) NOT NULL,
+  `Street` VARCHAR(45) NOT NULL,
+  `AptFloorSuite` VARCHAR(45) NULL DEFAULT NULL,
+  `City` VARCHAR(45) NOT NULL,
+  `State` VARCHAR(45) NOT NULL,
+  `Zip` VARCHAR(45) NOT NULL,
+  `Phone` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NOT NULL,
+  `Pref_contact` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -138,15 +120,29 @@ CREATE TABLE IF NOT EXISTS `PatientInfo`.`Form_Answers` (
   INDEX `fk_Form_Answers_Patient_Info1_idx` (`Patient_Info_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Form_Answers_Form_Questions1`
     FOREIGN KEY (`Form_Questions_ID`)
-    REFERENCES `PatientInfo`.`Form_Questions` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `PatientInfo`.`Form_Questions` (`ID`),
   CONSTRAINT `fk_Form_Answers_Patient_Info1`
     FOREIGN KEY (`Patient_Info_ID`)
-    REFERENCES `PatientInfo`.`Patient_Info` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `PatientInfo`.`Patient_Info` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `PatientInfo`.`Pin_Info`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PatientInfo`.`Pin_Info` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Pinname` VARCHAR(45) NOT NULL,
+  `Pin` VARCHAR(45) NOT NULL,
+  `Employee_Info_ID` INT NOT NULL,
+  PRIMARY KEY (`ID`, `Employee_Info_ID`),
+  INDEX `fk_Pin_Info_Employee_Info1_idx` (`Employee_Info_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_Pin_Info_Employee_Info1`
+    FOREIGN KEY (`Employee_Info_ID`)
+    REFERENCES `PatientInfo`.`Employee_Info` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
@@ -154,15 +150,14 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
-
 INSERT INTO Form_List VALUES
 (0,'GeneralInfo','Sam Dillon','2-13-2022', 1, 'first form, html, general info', 'for general patient info');
 
 INSERT INTO Employee_Info
 VALUES 
-(0,'Henry','Dare','exampleemail@domain.com','12/13/1999','nurseUser1','test','Nurse','123-456-7890','Male','22','N', '1'),
-(0,'Sam','Dillon','exampleemail2@domain.com','12/13/1999','doctorUser1','test','Doctor','246-810-1214','Male','22','N', '1'),
-(0,'Jared','Ormond','exampleemail3@domain.com','12/13/1999','adminUser1','test','Admin','135-791-1315','Male','22','N', '1');
+(0,'Henry','Dare','exampleemail@domain.com','12/13/1999','nurseUser1','test','Nurse','123-456-7890','Male','22','N', '1', 0),
+(0,'Sam','Dillon','exampleemail2@domain.com','12/13/1999','doctorUser1','test','Doctor','246-810-1214','Male','22','N', '1', 0),
+(0,'Jared','Ormond','exampleemail3@domain.com','12/13/1999','adminUser1','test','Admin','135-791-1315','Male','22','N', '1', 0);
 
 INSERT INTO PATIENT_INFO VALUES
 (0,'Henry','Dare','1234 Example Street Dr','','Example City','MI','48309','123-456-7891','email@exampledomain.com','Text'),
@@ -183,5 +178,5 @@ SELECT * FROM Form_List;
 
 SELECT * FROM Form_Answers;
 
-SELECT * FROM Patient_Info;
+SELECT * FROM Employee_Info;
 
